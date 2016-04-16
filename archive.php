79cb4bd3fc5df4cf62b70f9c146017349ef3e61a
Andreas Jönsson <adde90@gmail.com>
2016-04-16 11:39:31 +0200

    Adding output to archive template. Changed output of pages.

--- archive.php
+++ archive.php
@@ -13,3 +13,13 @@
  * @link http://codex.wordpress.org/Template_Hierarchy
  *
  */
+
+get_header(); ?>
+
+  <?php if(have_posts()): ?>
+    <?php while(have_posts()): the_post(); ?>
+      <?php get_template_part( 'content', 'standard' ); ?>
+    <?php endwhile; ?>
+  <?php endif; ?>
+
+<?php get_footer(); ?>
--- partials/page-standard.php
+++ partials/page-standard.php
@@ -10,7 +10,6 @@
 <article id="post-<?php the_ID(); ?>">
   <header>
     <h1><?php the_title(); ?></h1>
-    <small><?php the_date(); ?></small>
   </header>
   <section class="post-body">
     <?php the_content(); ?>
